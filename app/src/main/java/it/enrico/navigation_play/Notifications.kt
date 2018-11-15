bdb36c4786e7987b3cf59dfe3d9ad3e0d253899d
Enrico Bruno Del Zotto <enrico.delzotto@tui.com>
2018-11-15 16:07:42 +0100

    Fix gradle.properties to use androidX dep

--- app/src/main/java/it/enrico/navigation_play/Notifications.kt
+++ app/src/main/java/it/enrico/navigation_play/Notifications.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.NavUtils
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_notifications.*
